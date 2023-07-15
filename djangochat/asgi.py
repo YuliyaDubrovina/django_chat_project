1e138bc0e29a6e8b22eccef1ce5b58f7f845e1b0
Yuliya Dubrovina <jid081195@mail.ru>
2023-07-15 21:54:52 +0300

    Создание Log In, Log Out и Sign Up

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -1,19 +1,19 @@
-import os
+# import os
 
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
 
-import room.routing
+# import room.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            room.routing.websocket_urlpatterns
-        )
-    )
-})
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             room.routing.websocket_urlpatterns
+#         )
+#     )
+# })
